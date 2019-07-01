9ffd154aa8d8752bbd6746e63015911efeb3adea
unknown <isabellarevel>
2019-07-01 14:51:42 -0700

    data project woo!

--- U2-Applications/U2.1-Data/data_vis_project_starter.py
+++ U2-Applications/U2.1-Data/data_vis_project_starter.py
@@ -14,7 +14,42 @@ tweetData = json.load(tweetFile)
 tweetFile.close()
 
 # Continue your program below! 
+tweettext = []
+tweetstring = ""
+for tweet in tweetData:
+    tweetstring += tweet['text']
 
+for tweet in tweetData:
+    tweettext.append(tweet['text'])
+first = tweettext[10]
+print(type(first))
+
+print(tweetstring)
+blob_polarity = []
+for blob in tweettext:
+    b = TextBlob(blob)
+    blob_polarity.append(b.polarity)
+avg = sum(blob_polarity)/len(blob_polarity)
+
+blob_subjectivity = []
+for blob in tweettext:
+    blob_subjectivity.append(blob_subjectivity)
+worddict = {}
+
+tweetBlob = TextBlob(tweetstring)
+word_dict = {}
+for word in tweetBlob.words:
+    word_dict[word.lower()] = tweetBlob.word_counts[word.lower()]
+print(word_dict)
 # Textblob sample:
 tb = TextBlob("You are a brilliant computer scientist.")
-print(tb.polarity)
+print(tb.sentiment)
+
+
+plt.hist(blob_polarity, bins=[-1, -0.5, 0.0, 0.5, 1])
+plt.xlabel('Values')
+plt.ylabel('Number of Items')
+plt.title('Histogram of Numbers')
+plt.axis([-1.1, 1.1, 0, 100])
+plt.grid(True)
+plt.show()
